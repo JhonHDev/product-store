c3cfe985fbe0c43eac9579e4a60c3b9e18e3b2f5
Jhon Herrera <estebanhz9919@gmail.com>
2023-10-04 21:15:41 -0500

    [feat]: Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+	return (
+		<div className="text-center my-10">
+			<h1 className="text-2xl">Product Store</h1>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/app/ProductList/index.tsx
@@ -0,0 +1,7 @@
+import { Outlet } from 'react-router-dom';
+
+const ProductList = () => {
+	return <Outlet />;
+};
+
+export default ProductList;
